23e724519a6eb148354cbd4dab2abbd62be107cd
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-09-07 11:49:45 +0530

    API - Implement logout.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 const {
   authController: { login },
 } = require('../controllers');
+const { logout } = require('../controllers/authController');
 
 const authRouter = Router();
 
 authRouter.route('/login').post(login);
+authRouter.route('/logout').get(logout);
 
 module.exports = authRouter;
