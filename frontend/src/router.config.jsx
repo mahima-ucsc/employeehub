ea34437eec501cb5622b921d27fe27079e882b06
yomal7 <yomaloshada45@gmail.com>
2024-09-08 02:40:59 +0530

    UI - resolve conflicts

--- frontend/src/router.config.jsx
+++ frontend/src/router.config.jsx
@@ -11,6 +11,7 @@ import { EmployeeList } from "./features/employees/pages";
 import EmployeeEdit from "./features/employees/pages/employee-edit/employee-edit";
 import LeaveList from "./features/leaves/pages/leave-list/leave-list";
 import LeaveEdit from "./features/leaves/pages/leave-edit/leave-edit";
+import MyLeaveList from "./features/myleaves/pages/my-leaves-list/my-leaves-list";
 
 // eslint-disable-next-line no-unused-vars
 function ProtectedLayout() {
@@ -92,6 +93,10 @@ export default createBrowserRouter([
           {
             path: "employees/:userId/edit",
             element: <EmployeeEdit />,
+          },
+          {
+            path: "leaves/:userId/:leaveId/edit",
+            element: <LeaveEdit />,
           },
           {
             path: "leaves/:userId/:leaveId/edit",
