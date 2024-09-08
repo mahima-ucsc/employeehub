2034972309b63c1f6c5017aa0501bc05933c778b
Manupasan <manupasan321@gmail.com>
2024-09-08 14:57:35 +0530

    API-update notice by noticeID

--- backend/controllers/noticeController.js
+++ backend/controllers/noticeController.js
@@ -58,9 +58,30 @@ const deleteAllNotices = async (req, res) => {
     .json({ message: 'All notices have been deleted.' });
 };
 
+const updateNoticesById = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.noticeId))
+    throw new NotFoundError('Invalid notice id.');
+  let notice = await Notice.findOne({
+    _id: req.params.noticeId,
+  });
+
+  if (!notice) throw new NotFoundError('Invalid user id.');
+
+  notice.title = req.body.title || notice.title;
+  notice.description = req.body.description || notice.description;
+
+  const result = await notice.save();
+  res.status(StatusCodes.OK).json({
+    title: result.title,
+    description: result.description,
+    id: result._id,
+  });
+};
+
 module.exports = {
   createNotice,
   getNotices,
   getNoticesById,
   deleteAllNotices,
+  updateNoticesById,
 };
