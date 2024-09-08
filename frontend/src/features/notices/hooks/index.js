0e412e881820e7fef2fd7fe05d68ec2ab202bada
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-09-08 14:03:13 +0530

    Implement clean notice board.

--- frontend/src/features/notices/hooks/index.js
+++ frontend/src/features/notices/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useGetNotices } from "./get-notices";
 export { default as useCreateNotice } from "./create-notice";
+export { default as useCleanNoticeBoard } from "./delete-all-notices";
